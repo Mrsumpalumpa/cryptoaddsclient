86c642435ed1cbab07a2a0681ac8d43695dd6cd3
Mrsumpalumpa <marceloct1986@gmail>
2022-02-17 14:59:45 +0100

    fixed names xD

--- src/layouts/frontend/NavbarFront.js
+++ src/layouts/frontend/NavbarFront.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="sb-topnav navbar navbar-expand navbar-light bg-light">
             
-            <Link className="navbar-brand ps-3" to="/admin/"><img src={Logo} style={{maxWidth:'40px'}} alt=''></img> CryptoAdds</Link>
+            <Link className="navbar-brand ps-3" to="/admin/"><img src={Logo} style={{maxWidth:'40px'}} alt=''></img> CryptoAds</Link>
             
             <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" to="#!"><i className="fas fa-bars"></i></button>
             
